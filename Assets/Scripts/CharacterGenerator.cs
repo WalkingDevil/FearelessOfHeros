cb29cda4a0ebbe143cae1f5f2e23450076e3c532
akito52329 <sakuseimoto@gmail.com>
2022-07-06 14:09:11 +0900

    new

---
+++ Assets/Scripts/CharacterGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGenerator : MonoBehaviour
+{
+    [SerializeField] List<GameObject> geneObject;//生成するオブジェクト
+    [SerializeField] List<Transform> genePos;//生成する位置
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateChara(int num)
+    {
+
+    }
+}
