d28544d5d29caae1d75f071cd23c1343b717d66b
akito52329 <sakuseimoto@gmail.com>
2022-07-08 18:52:16 +0900

    new

--- Assets/Scripts/CharacterGenerator.cs
+++ Assets/Scripts/CharacterGenerator.cs
@@ -7,20 +7,30 @@ public class CharacterGenerator : MonoBehaviour
     [SerializeField] List<GameObject> geneObject;//生成するオブジェクト
     [SerializeField] List<Vector3> genePos;//生成する位置
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] float coolTime;
+    [SerializeField] float maxCoolTime;
     void Update()
     {
-        
+        if (maxCoolTime != 0)
+        {
+            if (coolTime < 0)
+            {
+                coolTime -= Time.deltaTime;
+            }
+            else
+            {
+                GenerateChara(GetRandom(genePos.Count));
+            }
+        }
     }
 
     public void GenerateChara(int num)
     {
-        Instantiate(geneObject[num], genePos[num], Quaternion.identity);
+        Instantiate(geneObject[num], genePos[0], Quaternion.identity);
+    }
+
+    private int GetRandom(int length)
+    {
+        return Random.Range(0, length);
     }
 }
