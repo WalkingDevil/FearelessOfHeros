2ef4a0337a169c1b2b171e0fba929f7c7bbdd12d
akito52329 <sakuseimoto@gmail.com>
2022-07-12 10:45:39 +0900

    new

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class TowerController : MonoBehaviour
 {
     [SerializeField] UserInterface userInterface;
     [SerializeField] string damageTag;
