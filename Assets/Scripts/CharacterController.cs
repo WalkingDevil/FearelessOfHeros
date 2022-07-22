2d3eea312adeebc837b66ccdfdeed97a1ea1d8ae
akito52329 <sakuseimoto@gmail.com>
2022-07-22 09:58:24 +0900

    new

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -41,13 +41,18 @@ public class CharacterController : MonoBehaviour
                 {
                     anime.TransitionAnime("die");
                     gameDirector.cost += plusCost;
-                    gameDirector.exp += userInterface.ExpCalculation();
+                    if (enemy)
+                    {
+                        gameDirector.exp += userInterface.ExpCalculation();
+                    }
+
                 }
             }
         }
 
     }
 
+    [SerializeField] bool enemy = false;//“G‚Å‚ ‚é‚©‚Ç‚¤‚©
     void Start()
     {
         anime.TransitionAnime("run");
