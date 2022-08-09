60f27a141c907871f5de8c50958c19e26e9110b5
akito52329 <sakuseimoto@gmail.com>
2022-08-10 01:17:18 +0900

    new

--- Assets/Scripts/LevelUpBonus.cs
+++ Assets/Scripts/LevelUpBonus.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelUpBonus : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private SaveData saveData;
+    private SavePath savePath;
+    private int startLevel = 0;
+    [SerializeField] RectTransform content;
+    [SerializeField] Text resultPrefab;
+    public LevelUpBonus(int startLevel = 0)
     {
-        
+        this.startLevel = startLevel;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        savePath = saveData.Load();
+    }
+
+    public void LevelBonus(int endLevel = 0)
+    {
+        int l = startLevel + 1;
+        for(;l <= endLevel;l++)
+        {
+            SetBonus(l);
+        }
+
+    }
+
+    private void SetBonus(int num)
+    {
+        switch(num)
+        {
+            case 5:
+                List<int> list = new List<int>(){ 5, 6, 7, 8 };
+                savePath.idData.AddRange(list);
+                GenerationText("54");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void GenerationText(string r)
+    {
+        Text text = Instantiate(resultPrefab, content.transform);
+        text.text = r;
     }
 }
