52905b5d21fab8ff0e798d2a331486c71387dfc7
Rukia <syaru223@gmail.com>
2022-10-28 14:10:28 +0900

    new

--- Assets/Scripts/Gacha/Gacha.cs
+++ Assets/Scripts/Gacha/Gacha.cs
@@ -8,23 +8,18 @@ public class Gacha : MonoBehaviour
 {
     [SerializeField] GachaLottery gachaTest;
     [SerializeField] GameObject cardPanel;
-    public List<int> character = new List<int>();
-    [SerializeField] private List<MonsterCard> monsterCards = new List<MonsterCard>();
-    public List<GameObject> objects = new List<GameObject>();
-    public List<MonsterCard> cards = new List<MonsterCard>();
-    static public int gachaCount = 10;
+    public List<int> character = new List<int>();  // ガチャの抽選結果
+    [SerializeField] private List<MonsterCard> monsterCards = new List<MonsterCard>();  // モンスターの種類
+    public List<GameObject> objects = new List<GameObject>();  // 抽選されたモンスター
+    public List<MonsterCard> cards = new List<MonsterCard>();  // 抽選されたモンスターカード
+    static public int gachaCount = 10;  //ガチャの回数
     // Start is called before the first frame update
     void Start()
     {
         GachaStart(gachaCount);
     }
 
-    void Update()
-    {
-        
-    }
-
-    public void GachaStart(int countPerGacha)
+    public void GachaStart(int countPerGacha)  //ガチャの抽選
     {
         Debug.Log(countPerGacha);
         character = gachaTest.Lottery(countPerGacha);
@@ -32,7 +27,7 @@ public class Gacha : MonoBehaviour
         {
             for (int j = 0; j < monsterCards.Count; j++)
             {
-                if (character[i] == monsterCards[j].GetState(0))
+                if (character[i] == monsterCards[j].GetState(0))  // 抽選結果をモンスターとモンスターカードのリストに入れる
                 {
                     objects.Add(monsterCards[j].GetObject().gameObject);
                     cards.Add(monsterCards[j]);
@@ -46,7 +41,7 @@ public class Gacha : MonoBehaviour
 
     IEnumerator Performance()
     {
-        foreach (GameObject obj in objects)
+        foreach (GameObject obj in objects)  // モンスターを表示
         {
             GameObject game = Instantiate(obj, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity) as GameObject;
             game.GetComponentInChildren<Canvas>().GetComponent<UserInterface>().ChengeGach();
@@ -62,7 +57,7 @@ public class Gacha : MonoBehaviour
         cardPanel.SetActive(true);
         yield return new WaitForSeconds(1f);
 
-        foreach (MonsterCard obj in cards)
+        foreach (MonsterCard obj in cards)  // モンスターカードを表示
         {
             MonsterCard card = Instantiate(obj) as MonsterCard;
             card.transform.parent = cardPanel.transform;
